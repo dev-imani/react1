df63243ddf7037eb79dd7edffd76f42ac00346bd
Kel Kel <michaelimani95@gmail.com>
2024-10-06 11:29:30 +0300

    new

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { CORE_CONCEPTS } from './data.js';
+import Header from './Components/Header/Header.jsx';
+import CoreConcept from './Components/CoreConcept.jsx';
+import TabButton from './Components/TabButton.jsx';
+
+function App() {
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <selection id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+        </selection>
+      </main>
+    </div>
+  );
+}
+
+export default App;
